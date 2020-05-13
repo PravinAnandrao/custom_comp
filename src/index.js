6b135ef71edcd5ea73c93c0d83fd8f1bf16a6ef3
Pravin Anandrao Bagade <pravin.bagade@mindtree.com>
2020-05-13 14:29:49 +0530

    added user stories

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const CustomButton = () => {
+  return (
+    <div>
+      <button type="button">Hi</button>
+    </div>
+  );
+}
+
+export default CustomButton;
