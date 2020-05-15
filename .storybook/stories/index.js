d070b62cd185c6076655dbd8b66990263d019854
Pravin Anandrao Bagade <pravin.bagade@mindtree.com>
2020-05-15 15:12:55 +0530

    updated the stories production build

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-
 import CustomButton from '../../src/index';
 
-storiesOf("Custom button", module).addWithJSX("simple", () => <CustomButton label={'Custom Button'} />);
+storiesOf("Button", module).add("with text", () => (
+  <CustomButton label={'Custom Button'} />
+));
